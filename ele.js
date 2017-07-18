7551f142d993f03506b28807c442671c5fa5bd46
Phil <zskullfox@gmail.com>
2017-07-19 00:13:55 +0200

    electron

--- ele.js
+++ ele.js
@@ -2,7 +2,7 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
-const bepoApp = require('../bepo.js')
+const bepoApp = require('./bepo.js')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
